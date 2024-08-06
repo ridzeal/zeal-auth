e59cc36c445ef8a3f93a170f9dd052e7c18862f6
Riadhi Aditya <ridzeal@gmail.com>
2024-08-06 21:56:06 +0700

    Try adding handler to main.go

--- main.go
+++ main.go
@@ -1,7 +1,10 @@
 package main
 
 import (
+	"fmt"
+	"net/http"
 	"sso-backend/handlers"
+
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
 )
@@ -20,4 +23,8 @@ func main() {
 
 	// Start server
 	e.Logger.Fatal(e.Start(":3000"))
+}
+
+func Handler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "<h1>Hello from Go!</h1>")
 }
